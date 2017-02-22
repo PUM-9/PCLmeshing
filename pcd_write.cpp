c581d4c391af6eb3d317c99f63d1c9dbc6f59454
Martin <martin95lundberg@gmail.com>
2017-02-22 12:08:43 +0100

    Can somewhat mesh point clouds

---
+++ pcd_write.cpp
@@ -0,0 +1,113 @@
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/surface/gp3.h>
+
+#include <pcl/io/vtk_io.h>
+#include <pcl/io/vtk_lib_io.h>
+#include <iostream>
+#include <pcl/filters/passthrough.h>
+
+#include <pcl/search/kdtree.h>
+#include <pcl/search/impl/kdtree.hpp>
+#include <pcl/kdtree/impl/kdtree_flann.hpp>
+#include <pcl/search/organized.h>
+#include <pcl/search/impl/organized.hpp>
+#include <pcl/surface/gp3.h>
+#include <pcl/surface/impl/gp3.hpp>
+#include <pcl/features/normal_3d.h>
+#include <pcl/features/impl/normal_3d.hpp>
+#include <pcl/filters/impl/passthrough.hpp>
+
+int main (int argc, char** argv)
+{
+  std::cout << "Loading file" << std::endl;
+  
+  // Load input file into a PointCloud<T> with an appropriate type
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PCLPointCloud2 cloud_blob;
+  if (pcl::io::loadPCDFile ("bun0.pcd", cloud_blob) == -1) {
+    std::cout << "Couldn't read file bun0.pcd" << std::endl;
+  }
+  pcl::fromPCLPointCloud2 (cloud_blob, *cloud);
+  //* the data should be available in cloud
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered_first(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
+
+  // Create the filtering object
+  pcl::PassThrough<pcl::PointXYZ> pass;
+  pass.setInputCloud (cloud);
+  pass.setFilterFieldName ("x");
+  pass.setFilterLimits (0.0, 1.0);
+  pass.setFilterLimitsNegative (true);
+  pass.filter (*cloud_filtered_first);
+
+  pass.setInputCloud(cloud_filtered_first);
+  pass.setFilterFieldName("z");
+  pass.setFilterLimits(-10, 10);
+  pass.setFilterLimitsNegative(true);
+  pass.filter(*cloud_filtered);
+  
+  std::cout << "Saving filtered cloud" << std::endl;
+  pcl::io::savePCDFile("bun0_filtered.pcd", *cloud_filtered);
+  
+  std::cout << "Estimating normals" << std::endl;
+  // Normal estimation*
+  pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> n;
+  pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
+  pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
+  tree->setInputCloud (cloud_filtered);
+  n.setInputCloud (cloud_filtered);
+  n.setSearchMethod (tree);
+  n.setKSearch (20);
+  n.compute (*normals);
+  //* normals should not contain the point normals + surface curvatures
+
+  // Concatenate the XYZ and normal fields*
+  pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals (new pcl::PointCloud<pcl::PointNormal>);
+  pcl::concatenateFields (*cloud_filtered, *normals, *cloud_with_normals);
+  //* cloud_with_normals = cloud + normals
+
+  // Create search tree*
+  pcl::search::KdTree<pcl::PointNormal>::Ptr tree2 (new pcl::search::KdTree<pcl::PointNormal>);
+  tree2->setInputCloud (cloud_with_normals);
+
+  // Initialize objects
+  pcl::GreedyProjectionTriangulation<pcl::PointNormal> gp3;
+  pcl::PolygonMesh triangles;
+
+  // Set the maximum distance between connected points (maximum edge length)
+  gp3.setSearchRadius (0.1);
+
+  // Set typical values for the parameters
+  gp3.setMu (2.5);
+  gp3.setMaximumNearestNeighbors (100);
+  gp3.setMaximumSurfaceAngle(M_PI/4); // 45 degrees
+  gp3.setMinimumAngle(M_PI/18); // 10 degrees
+  gp3.setMaximumAngle(2*M_PI/3); // 120 degrees
+  gp3.setNormalConsistency(false);
+
+  std::cout << "Reconstructing" << std::endl;
+
+  // Get result
+  gp3.setInputCloud (cloud_with_normals);
+  gp3.setSearchMethod (tree2);
+  gp3.reconstruct (triangles);
+  
+  // Additional vertex information
+  std::vector<int> parts = gp3.getPartIDs();
+  std::vector<int> states = gp3.getPointStates();
+
+  pcl::io::savePolygonFile("mesh.stl", triangles);
+  
+  if (pcl::io::savePolygonFile("mesh.vtk", triangles)) {
+    std::cout << "Saved file successfully" << std::endl;
+  } else {
+    std::cout << "Failed to save file" << std::endl;
+  }
+	      
+  // Finish
+  return (0);
+}
